42ed9c5109da1e51f4d743d4a31f315b5cbe4116
Ondrej Certik <ondrej@certik.cz>
2009-02-01 15:24:39 -0800

    Handle logins well

--- chess/views.py
+++ chess/views.py
@@ -12,6 +12,7 @@ from chess.models import Game, PGNFile
 from ragendja.template import render_to_response, render_to_string
 
 from google.appengine.api import users
+from google.appengine.ext import db
 
 def render_to_response(tempname, dictionary):
     """
@@ -39,21 +40,30 @@ def login_required(func):
 
 @login_required
 def list_games(request):
+    u = users.get_current_user()
+    games = db.Query(Game).filter("owner", u)
+    pgn_files = db.Query(PGNFile).filter("owner", u)
     nick = users.get_current_user().nickname()
-    return render_to_response("game_list.html",
-            {"games": Game.all(), "pgn_files": PGNFile.all(),
-                 "login": nick})
+    return render_to_response("game_list.html", {
+        "games": games,
+        "pgn_files": pgn_files,
+        "login": nick,
+        })
     #return object_list(request, Game.all(), paginate_by=10)
 
+@login_required
 def show_game(request, key):
     return object_detail(request, Game.all(), key)
 
+@login_required
 def create_game(request):
     return create_object(request, Game)
 
+@login_required
 def edit_game(request, key):
     return update_object(request, Game, key)
 
+@login_required
 def delete_game(request, key):
     return delete_object(request, Game, object_id=key,
         post_delete_redirect=reverse('chess.views.list_games'))
@@ -62,21 +72,25 @@ class UploadFileForm(forms.Form):
     #title = forms.CharField(max_length=50)
     file = forms.FileField()
 
+@login_required
 def upload_pgn(request):
     if request.method == "POST":
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
             f = request.FILES['file']
-            p = PGNFile(filename=f.name, filecontent=f.read())
+            p = PGNFile(filename=f.name, filecontent=f.read(),
+                    owner=users.get_current_user())
             p.put()
             return HttpResponseRedirect(reverse("chess.views.list_games"))
     else:
         form = UploadFileForm()
     return render_to_response("pgnfile_form.html", {"form": form})
 
+@login_required
 def show_pgn_file(request, key):
     return object_detail(request, PGNFile.all(), key)
 
+@login_required
 def delete_pgn_file(request, key):
     return delete_object(request, PGNFile, object_id=key,
         post_delete_redirect=reverse('chess.views.list_games'))
