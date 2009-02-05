6e934aaf5ef19a566b1e4307e6bfa7b283568b25
Ondrej Certik <ondrej@certik.cz>
2009-02-04 23:15:21 -0800

    more test

--- chess/board/board.py
+++ chess/board/board.py
@@ -348,22 +348,12 @@ class Pawn(Piece):
         return False
 
 def main():
+    moves = ['e4', 'e5', 'Nf3', 'Nc6', 'Bb5', 'a6', 'Ba4', 'Nf6', 'O-O', 'Be7', 'Re1', 'b5', 'Bb3', 'd6', 'c3', 'O-O', 'h3', 'Nb8', 'd4', 'Nbd7', 'c4', 'c6', 'cxb5', 'axb5', 'Nc3', 'Bb7', 'Bg5', 'b4', 'Nb1', 'h6', 'Bh4', 'c5', 'dxe5', 'Nxe4', 'Bxe7', 'Qxe7', 'exd6', 'Qf6', 'Nbd2', 'Nxd6', 'Nc4', 'Nxc4', 'Bxc4', 'Nb6', 'Ne5', 'Rae8', 'Bxf7+', 'Rxf7', 'Nxf7', 'Rxe1+', 'Qxe1', 'Kxf7', 'Qe3', 'Qg5', 'Qxg5', 'hxg5', 'b3', 'Ke6', 'a3', 'Kd6', 'axb4', 'cxb4', 'Ra5', 'Nd5', 'f3', 'Bc8', 'Kf2', 'Bf5', 'Ra7', 'g6', 'Ra6+', 'Kc5', 'Ke1', 'Nf4', 'g3', 'Nxh3', 'Kd2', 'Kb5', 'Rd6', 'Kc5', 'Ra6', 'Nf2', 'g4', 'Bd3', 'Re6']
+    print moves[20]
+    moves = moves[:19]
     b = Board()
-    b.move_algebraic("e4")
-    b.move_algebraic("e5")
-    b.move_algebraic("Nf3")
-    b.move_algebraic("Nc6")
-    b.move_algebraic("Bb5")
-    b.move_algebraic("a6")
-    b.move_algebraic("Bxc6")
-    b.move_algebraic("dxc6")
-    b.move_algebraic("d3")
-    b.move_algebraic("Bb4+")
-    b.move_algebraic("Nc3")
-    b.move_algebraic("Nf6")
-    b.move_algebraic("O-O")
-    b.move_algebraic("Bxc3")
-    b.move_algebraic("Re1")
+    for move in moves:
+        b.move_algebraic(move)
     print b
     print b.to_string()
 
