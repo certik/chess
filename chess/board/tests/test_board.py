f684f0ab8cfac61a7b99973dd883f7a50d81012b
Ondrej Certik <ondrej@certik.cz>
2009-02-04 22:35:13 -0800

    Tests improved

--- chess/board/tests/test_board.py
+++ chess/board/tests/test_board.py
@@ -10,3 +10,15 @@ def test_setup2():
     b.move_algebraic("e4")
     assert b.to_string() ==  \
         "RNBQKBNRPPPPPPPP                    p           pppp ppprnbqkbnr"
+
+def test_setup3():
+    b = Board()
+    b.move_algebraic("e4")
+    b.move_algebraic("e5")
+    b.move_algebraic("Nf3")
+    b.move_algebraic("Nc6")
+    b.move_algebraic("Bb5")
+    b.move_algebraic("a6")
+    b.move_algebraic("Bxc6")
+    assert b.to_string() ==  \
+        "R BQKBNR PPP PPPP b         P       p        n  pppp ppprnbqk  r"
