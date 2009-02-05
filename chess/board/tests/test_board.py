2554da7324ee43db4df1d04a3f7bc56af168e013
Ondrej Certik <ondrej@certik.cz>
2009-02-04 23:02:43 -0800

    More moves added

--- chess/board/tests/test_board.py
+++ chess/board/tests/test_board.py
@@ -35,3 +35,23 @@ def test_moves3():
     b.move_algebraic("dxc6")
     assert b.to_string() ==  \
         "R BQKBNR PP  PPPP P         P       p        n  pppp ppprnbqk  r"
+
+def test_moves4():
+    b = Board()
+    b.move_algebraic("e4")
+    b.move_algebraic("e5")
+    b.move_algebraic("Nf3")
+    b.move_algebraic("Nc6")
+    b.move_algebraic("Bb5")
+    b.move_algebraic("a6")
+    b.move_algebraic("Bxc6")
+    b.move_algebraic("dxc6")
+    b.move_algebraic("d3")
+    b.move_algebraic("Bb4+")
+    b.move_algebraic("Nc3")
+    b.move_algebraic("Nf6")
+    b.move_algebraic("O-O")
+    b.move_algebraic("Bxc3")
+    b.move_algebraic("Re1")
+    assert b.to_string() ==  \
+        "R BQK  R PP  PPPP P  N      P       p     Bp n  ppp  pppr bqr k "
