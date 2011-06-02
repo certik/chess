3e98f8177020eb665ed1ac946db573f77aeaad54
Ondrej Certik <ondrej@certik.cz>
2011-06-02 01:54:29 -0700

    Fix the UCI interface to work

--- py_uci.py
+++ py_uci.py
@@ -10,32 +10,32 @@ class UCIEngine(object):
         self._p.sendline("uci")
         self._p.sendline("setoption name Hash value 1024")
         self._p.expect("uciok")
+
+    def new_game(self):
         self._p.sendline("ucinewgame")
         self._p.sendline("isready")
         self._p.expect("readyok")
 
-    def new_game(self):
-        self._p.sendline("position startpos")
-
-    def make_move(self, move):
+    def set_position(self, initial_position=None, moves=None):
         """
-        Make the move 'move' on the internal chessboard.
+        Sets the position on the internal chessboard.
 
-        The move has to be a valid move, otherwise the engine can segfault
-        later on.
+        initial_position ... If None, start position is used, otherwise specify
+                             a position in a "fen" format.
+        moves ... List of moves to make, in algebraic notation from
+                  "initial_position". Moves have to valid, otherwise the engine
+                  can segfault later on.
         """
-        self._p.sendline("position moves %s" % move)
-
-    def make_moves(self, moves):
-        """
-        Make the move 'moves' on the internal chessboard.
-
-        'moves' is a list of moves.
-
-        The move has to be a valid move, otherwise the engine can segfault
-        later on.
-        """
-        self._p.sendline("position moves %s" % " ".join(moves))
+        if moves:
+            assert len(moves) >= 1
+            s_moves = "moves " + " ".join(moves)
+        else:
+            s_moves = ""
+        if initial_position:
+            self._p.sendline("position fen %s %s" % (initial_position,
+                s_moves))
+        else:
+            self._p.sendline("position startpos %s" % s_moves)
 
     def find_best_move(self, movetime=2):
         if movetime is None:
@@ -59,6 +59,7 @@ if __name__ == "__main__":
     e.new_game()
     ponder = "e2e4"
     i = 0
+    moves = []
     while 1:
         i += 1
         if i < 30:
@@ -66,7 +67,8 @@ if __name__ == "__main__":
         else:
             move = get_move_from_user(ponder)
         print "you:", move
-        e.make_move(move)
+        moves.append(move)
+        e.set_position(moves=moves)
         best_move, ponder = e.find_best_move()
-        e.make_move(best_move)
+        moves.append(best_move)
         print "computer:", best_move
